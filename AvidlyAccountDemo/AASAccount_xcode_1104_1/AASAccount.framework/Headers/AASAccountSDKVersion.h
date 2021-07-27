9788e5d6e930b9e0fcd019b4d33015011de0cf5d
steve <909476821@qq.com>
2021-07-27 16:52:30 +0800

    1.1.0.4

---
+++ AvidlyAccountDemo/AASAccount_xcode_1104_1/AASAccount.framework/Headers/AASAccountSDKVersion.h
@@ -0,0 +1,11 @@
+//
+//  AccountSDKVersion.h
+//  AccountSDK
+//
+//  Created by steve on 2019/1/24.
+//  Copyright © 2019 Technology Co.,Ltd. All rights reserved.
+//
+
+#define AASAccountSDK_Version   @"1.1.0.4"
+#define AASAccountSDK_Build     @"1"
+#define AASAccountSDKVersionString      @"AASDK_VERSION_1104.1"
